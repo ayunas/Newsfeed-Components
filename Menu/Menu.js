18b72b01a66cd54580f6b2a8e866adcc6bfc7921
ayunas <ayunas@gmail.com>
2019-03-16 11:07:42 -0500

    added menu popup functionality

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,47 @@
+//click on the hamburger, and menu drops down.  
 
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+const hamburger = document.querySelector('.menu-button');
+
+const menu = document.querySelector('.menu');
+
+console.log(menu);
+
+function toggleMenu() {
+//    menu.style.display = 'block';
+    menu.classList.toggle('menu--open');
 }
 
-// Start Here: Create a reference to the ".menu" class
-const menu;
-// create a reference to the ".menu-button" class
-const menuButton;
-// Using your menuButton reference, add a click handler that calls toggleMenu
+hamburger.addEventListener('click',() => toggleMenu());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const toggleMenu = () => {
+//  // Toggle the "menu--open" class on your menu refence. 
+//}
+//
+//// Start Here: Create a reference to the ".menu" class
+//const menu;
+//// create a reference to the ".menu-button" class
+//const menuButton;
+//// Using your menuButton reference, add a click handler that calls toggleMenu
